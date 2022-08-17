9e83fed9530b7e3c1edfe66bfd0fb3b0d4cda8e8
Kevin Fowler <kevin.m.fowler17@gmail.com>
2022-08-17 16:56:51 -0500

    loops exercise finished

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,3 +1,32 @@
-function showMultiplicationTable(number){
+"use strict";
+// let total = 0;
+//
+// function showMultiplicationTable(number){
+//     for (let i = 1; i <= 10; i++) {
+//         total = number * i;
+//         console.log(`${number} x ${i} = ${total}`);
+//     }
+// }
 
+// let randomNumber = Math.floor(Math.random() * 20) + 180;
+// for (let i = 1; i <= 10; i++){
+//     if (randomNumber % 2 === 0) {
+//         console.log(`${randomNumber} is even`);
+//     } else {
+//         console.log(`${randomNumber} is odd`);
+//     }
+//     randomNumber = Math.floor(Math.random() * 20) + 180;
+// }
+
+for (let i = 1; i <= 9; i++) {
+    let row = [];
+    for (let a = 1; a <= i; a++) {
+        row.push(i);
+    }
+    console.log(row.join(""));
 }
+
+
+// for (let i = 100; i >= 5; i -= 5){
+//     console.log(i);
+// }
