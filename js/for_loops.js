f1a9599ff29b671a2a294aab646a56729b4658d6
Kevin Fowler <kevin.m.fowler17@gmail.com>
2022-08-18 11:33:29 -0500

    adding breaks and continues exercise

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,32 +1,63 @@
 "use strict";
-// let total = 0;
-//
 // function showMultiplicationTable(number){
 //     for (let i = 1; i <= 10; i++) {
-//         total = number * i;
-//         console.log(`${number} x ${i} = ${total}`);
+//         console.log(`${number} x ${i} = ${number * i}`);
 //     }
 // }
-
-// let randomNumber = Math.floor(Math.random() * 20) + 180;
+//
+//
 // for (let i = 1; i <= 10; i++){
+//     let randomNumber = Math.floor(Math.random() * 181) + 20;
 //     if (randomNumber % 2 === 0) {
 //         console.log(`${randomNumber} is even`);
 //     } else {
 //         console.log(`${randomNumber} is odd`);
 //     }
-//     randomNumber = Math.floor(Math.random() * 20) + 180;
 // }
-
-for (let i = 1; i <= 9; i++) {
-    let row = [];
-    for (let a = 1; a <= i; a++) {
-        row.push(i);
-    }
-    console.log(row.join(""));
-}
-
-
+//
+// for (let i = 1; i <= 9; i++) {
+//     let row = [];
+//     for (let a = 1; a <= i; a++) {
+//         row.push(i);
+//     }
+//     console.log(row.join(""));
+// }
+//
+// for (let i = 1; i <= 9; i++) {
+//     console.log(i.toString().repeat(i));
+// }
+//
+// for (let i = 1; i <= 9; i++) {
+//     let outputString = "";
+//     for (let count = 1; count < i; count++) {
+//         outputString = outputString + i;
+//     }
+//     console.log(outputString);
+// }
+//
 // for (let i = 100; i >= 5; i -= 5){
 //     console.log(i);
 // }
+
+// let string = "";
+// for (let i = 0; i < 8; i++){
+//     let word = prompt("Lets make a sentence: Enter a word: ");
+//     string += " " + word;
+//     if (word === "curses"){
+//         console.log("That word is not allowed.");
+//         break;
+//     }
+//     console.log("Your sentence is:" + string);
+//
+// }
+
+let string = "";
+for (let i = 0; i < 8; i++){
+    let word = prompt("Lets make a sentence: Enter a word: ");
+    if (word === "curses"){
+        console.log("That word is not allowed.");
+        continue;
+    }
+    string += " " + word;
+    console.log("Your sentence is:" + string);
+}
