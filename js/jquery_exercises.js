341dab6592d33be7ddd71f7f1f1ec9c9cac498c9
Kevin Fowler <kevin.m.fowler17@gmail.com>
2023-01-13 10:04:49 -0600

    jquery exercise - part one

---
+++ js/jquery_exercises.js
@@ -0,0 +1,15 @@
+"use strict";
+
+$(function () {
+
+    console.log(jQuery.fn.jquery);
+
+    console.log($.fn.jquery);
+
+});
+
+$(function() {
+
+    alert( 'The DOM has finished loading!' );
+
+});
