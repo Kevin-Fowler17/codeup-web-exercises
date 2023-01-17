2f16551e4d65b907c04d6ca6d3c1c2e84c9041ee
Kevin Fowler <kevin.m.fowler17@gmail.com>
2023-01-17 10:32:43 -0600

    jquery exercise - mouse events

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -4,11 +4,11 @@
 
 // $(".codeup").css("border", "1px solid red");
 
-$("li").css("font-size", "20px");
-
-$("h1, p, li").css("background-color", "yellow");
-
-alert($('h1').html());
+// $("li").css("font-size", "20px");
+//
+// $("h1, p, li").css("background-color", "yellow");
+//
+// alert($('h1').html());
 
 // $("*").css("background-color", "yellow");
 
@@ -29,3 +29,43 @@ alert($('h1').html());
 // $(document).ready(function (){
 //     alert( 'The DOM has finished loading! Part deux' )
 // });
+
+
+
+// Add jQuery code that will change the background color of an h1 element when clicked.
+$("h1").click(
+    function() {
+        $(this).css("background-color", "cornflowerblue");
+    }
+)
+
+
+// Make all paragraphs have a font size of 18px when they are double clicked.
+// $("p").dblclick(
+//     function () {
+//         $("p").css("font-size", "18px")
+//     }
+// )
+$("p").dblclick(
+    function () {
+        $(this).css("font-size", "18px")
+    }
+)
+
+
+// Set all li text color to red when the mouse is hovering; reset to black when it is not.
+$("li").hover(hoverOn, hoverOff)
+
+// function hoverOn() {
+//     $("li").css("color", "red")
+// }
+// function hoverOff() {
+//     $("li").css("color", "black")
+// }
+
+function hoverOn() {
+    $(this).css("color", "red")
+}
+function hoverOff() {
+    $(this).css("color", "black")
+}
