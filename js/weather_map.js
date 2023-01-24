6cd424b2a5419c35e8ca7a96168b2070a5807f18
Kevin Fowler <kevin.m.fowler17@gmail.com>
2023-01-24 11:34:33 -0600

    weather map v3

--- js/weather_map.js
+++ js/weather_map.js
@@ -10,16 +10,14 @@ function startingWeatherAndMap () {
         lon:   -98.40349,
         units: "imperial"
     }).done(function(data) {
-        // console.log(data);
-        // console.log(data.list[0].dt_txt)
-        // const weatherData = [...data];
-        // console.log(weatherData)
-        // console.log(weatherData.current.dt);
-        // console.log('The entire response:', data);
-        // console.log('Diving in - here is current information: ', data.current);
-        // console.log('A step further - information for tomorrow: ', data.daily[1]);
 
-    document.getElementById("currentCity").innerHTML = 'Curreny city: ' + data.city.name;
+    document.getElementById("currentCity").innerHTML = 'Current city: ' + data.city.name;
+
+    document.getElementById("tip1").innerHTML = renderWeatherTip(data, 0);
+    document.getElementById("tip2").innerHTML = renderWeatherTip(data, 8);
+    document.getElementById("tip3").innerHTML = renderWeatherTip(data,16);
+    document.getElementById("tip4").innerHTML = renderWeatherTip(data,24);
+    document.getElementById("tip5").innerHTML = renderWeatherTip(data,32);
 
     document.getElementById("card1").innerHTML = renderWeather(data, 0);
     document.getElementById("card2").innerHTML = renderWeather(data, 8);
@@ -47,13 +45,6 @@ function startingWeatherAndMap () {
         .setHTML(`<br>San Antonio, TX`)
     marker.setPopup(popup)
 
-    function onDragEnd() {
-        const lngLat = marker.getLngLat();
-        coordinates.style.display = 'block';
-        coordinates.innerHTML = `Longitude: ${lngLat.lng}<br />Latitude: ${lngLat.lat}`;
-    }
-
-    marker.on('dragend', onDragEnd);
 }
 
 startingWeatherAndMap();
@@ -72,11 +63,14 @@ function updateWeatherAndLocation () {
             units: "imperial"
         }).done(function(data) {
             console.log(data);
-            // console.log('The entire response:', data);
-            // console.log('Diving in - here is current information: ', data.current);
-            // console.log('A step further - information for tomorrow: ', data.daily[1]);
 
             document.getElementById("currentCity").innerHTML = 'Curreny city: ' + data.city.name;
+
+            document.getElementById("tip1").innerHTML = renderWeatherTip(data, 0);
+            document.getElementById("tip2").innerHTML = renderWeatherTip(data, 8);
+            document.getElementById("tip3").innerHTML = renderWeatherTip(data,16);
+            document.getElementById("tip4").innerHTML = renderWeatherTip(data,24);
+            document.getElementById("tip5").innerHTML = renderWeatherTip(data,32);
 
             document.getElementById("card1").innerHTML = renderWeather(data, 0);
             document.getElementById("card2").innerHTML = renderWeather(data, 8);
@@ -89,7 +83,7 @@ function updateWeatherAndLocation () {
         const map = new mapboxgl.Map({
             container: 'my-map',
             style: 'mapbox://styles/mapbox/satellite-streets-v12',
-            zoom: 20,
+            zoom: 10,
             center: [results[0], results[1]]
         });
 
@@ -107,23 +101,34 @@ function updateWeatherAndLocation () {
 
         marker.on('dragend', onDragEnd)
 
-
-
-
         let popup = new mapboxgl.Popup()
             .setHTML(`<br>${addAddressSelection.value}`)
         marker.setPopup(popup)
     })
 }
 
-function renderWeather(data, increment) {
+function renderWeatherTip(data, increment) {
 
+    console.log("test")
+    let html = '<ul>';
+    html += '<li>Feels Like: ' + Math.round(data.list[increment].main.feels_like) + String.fromCharCode(176) + 'F</li>';
+    html += '<li>Humidity: ' + Math.round(data.list[increment].main.humidity) + '%</li>';
+    html += '<li>Cloud Coverage: ' + Math.round(data.list[increment].clouds) + '%</li>';
+    html += '<li>Wind Speed: ' + Math.round(data.list[increment].wind.speed) + '%</li>';
+    html += '</ul>';
+    html += '</span>';
+
+    return html;
+}
+
+function renderWeather(data, increment) {
+    console.log("test 2")
     let formattedDate = new Date((data.list[increment].dt * 1000))
 
-    let html = '<div class="test mx-auto card-header">' + formattedDate.toDateString() + '</div>';
+    let html = '<div class="mx-auto card-header">' + formattedDate.toDateString() + '</div>';
     html += '<ul class="list-group list-group-flush">';
     html += '<li class="list-group-item"><img src="http://openweathermap.org/img/wn/' + data.list[increment].weather[0].icon + '@2x.png" style="width: 50px;"/></li>';
-    html += '<li class="list-group-item">' + Math.round(data.list[increment].main.temp) + String.fromCharCode(176) + 'F ' + data.list[0].weather[0].main + '</li>';
+    html += '<li class="list-group-item">' + Math.round(data.list[increment].main.temp) + String.fromCharCode(176) + 'F ' + data.list[increment].weather[0].main + '</li>';
     html += '<li class="list-group-item">H: ' + Math.round(data.list[increment].main.temp_max) + String.fromCharCode(176) + 'F / L:' + Math.round(data.list[0].main.temp_min) + String.fromCharCode(176) + 'F</li><br>';
     html += '</ul>';
 
