d4b031e5cc5ce2c3d26914cf17d5941f1a3efdfe
Kevin Fowler <kevin.m.fowler17@gmail.com>
2023-01-30 15:44:58 -0600

    promises exercise

--- js/promises.js
+++ js/promises.js
@@ -1,3 +1,38 @@
-"use strict"
+(function() {
+    "use strict"
+    // Create a function that accepts a GitHub username, and returns a promise that resolves returning just the
+    // date of the last commit that user made. Reference the github api documentation to achieve this.
+    function getLastPush(user){
+        return fetch(`https://api.github.com/users/${user}/events/public`, {
+            headers: {
+                'Authorization': 'token ' + MY_GITHUB_API_KEY
+            }
+        }).then(response => {
+            return response.json()
+        })
+            .then(events => {
+                const dateOfLastPush = new Date(events[0].created_at).toDateString();
+                console.log(dateOfLastPush);
+                return dateOfLastPush;
+            });
+    }
 
-fetch(url, {headers: {'Authorization': 'token YOUR_TOKEN_HERE'}})
+    getLastPush('Kevin-Fowler17')
+
+    //Write a function named wait that accepts a number as a parameter, and returns a
+    // promise that resolves after the passed number of milliseconds.
+    function wait(time) {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve(time);
+            }, time);
+        });
+    }
+
+    wait(1000).then((msg) => console.log("You'll see this after 1 second: " + msg + " milliseconds"));
+    wait(3000).then((msg) => console.log("You'll see this after 3 seconds: " + msg + " milliseconds"));
+
+
+})();
+
+
