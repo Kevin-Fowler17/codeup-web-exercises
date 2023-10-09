e133957974ad9a98215d8bf871ce2e370c34e5f4
Kevin Fowler <kevin.m.fowler17@gmail.com>
2023-10-09 16:20:33 -0500

    hacker rank 10 days of javascript solutions

---
+++ js/10_days_of_javascript_solutions.js
@@ -0,0 +1,13 @@
+//Day 0
+
+// Write a line of code that prints parameterVariable to stdout using console.log:
+console.log(parameterVariable);
+
+// Write code that uses console.log to print the sum of the 'firstInteger' and 'secondInteger' (converted to a Number        type) on a new line.
+console.log (firstInteger + Number(secondInteger))
+
+// Write code that uses console.log to print the sum of 'firstDecimal' and 'secondDecimal' (converted to a Number            type) on a new line.
+console.log (firstDecimal + parseFloat(secondDecimal))
+
+// Write code that uses console.log to print the concatenation of 'firstString' and 'secondString' on a new line. The        variable 'firstString' must be printed first.
+console.log (firstString + secondString)
