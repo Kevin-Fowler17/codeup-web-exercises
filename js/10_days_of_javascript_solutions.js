1e5730ceafb0e9670b4824cf97bd8edb8ddcdd87
Kevin Fowler <kevin.m.fowler17@gmail.com>
2023-10-10 15:35:41 -0500

    hacker rank 10 days of javascript solutions - day 2 done

--- js/10_days_of_javascript_solutions.js
+++ js/10_days_of_javascript_solutions.js
@@ -9,6 +9,7 @@ console.log (firstDecimal + parseFloat(secondDecimal))
 // Write code that uses console.log to print the concatenation of 'firstString' and 'secondString' on a new line. The        variable 'firstString' must be printed first.
 console.log (firstString + secondString)
 
+
 // Day 1
 // Return a number denoting the rectangle's area.
 function getArea(length, width) {
@@ -39,6 +40,48 @@ function factorial(n) {
     return total;
 }
 
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function main() {
+    // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+    const PI = Math.PI;
+    let r = parseFloat(readLine());
+    // Print the area of the circle:
+    console.log (PI * (r * r));
+    // Print the perimeter of the circle:
+    console.log (2 * PI * r);
+    try {
+        // Attempt to redefine the value of constant variable PI
+        PI = 0;
+        // Attempt to print the value of PI
+        console.log(PI);
+    } catch(error) {
+        console.error("You correctly declared 'PI' as a constant.");
+    }
+}
+
 
 // Day 2
 /*
@@ -60,3 +103,94 @@ function vowelsAndConsonants(s) {
     }
 
 }
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function getGrade(score) {
+    let grade;
+    // Write your code here
+    if (score >= 26) {
+        grade = "A";
+    } else if (score >= 21 && score <= 25) {
+        grade = "B";
+    } else if (score >= 16 && score <= 20) {
+        grade = "C";
+    } else if (score >= 11 && score <= 15) {
+        grade = "D";
+    } else if (score >= 6 && score <= 10) {
+        grade = "E";
+    } else {
+        grade = "F";
+    }
+
+    return grade;
+}
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function getLetter(s) {
+    let letter;
+    // Write your code here
+    switch(true) {
+        case "aeiou".indexOf(s.charAt(0)) !== -1:
+            letter = "A"
+            break;
+        case "bcdfg".indexOf(s.charAt(0)) !== -1:
+            letter = "B"
+            break;
+        case "hjklm".indexOf(s.charAt(0)) !== -1:
+            letter = "C"
+            break;
+        default:
+            letter = "D";
+    }
+
+    return letter;
+}
+
+
+// Day 3
