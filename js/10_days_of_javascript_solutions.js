a9f7e80a489da08030ab3137ec085c349a4e62ad
Kevin Fowler <kevin.m.fowler17@gmail.com>
2023-10-12 16:21:19 -0500

    hacker rank 10 days of javascript solutions - day 6 is done

--- js/10_days_of_javascript_solutions.js
+++ js/10_days_of_javascript_solutions.js
@@ -347,3 +347,68 @@ function modifyArray(nums) {
 
 
 // Day 6
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+function getMaxLessThanK(n, k) {
+    let max = 0;
+
+    for (let a = 1; a <= n; a++) {
+        for (let b = a + 1; b <= n; b++) {
+            let ab = (a&b);
+
+            if (ab < k && ab > max) {
+                max = ab;
+            }
+        }
+    }
+    return max;
+}
+
+function main() {
+    const q = +(readLine());
+
+    for (let i = 0; i < q; i++) {
+        const [n, k] = readLine().split(' ').map(Number);
+
+        console.log(getMaxLessThanK(n, k));
+    }
+}
+
+// The days of the week are: "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+function getDayName(dateString) {
+    let dayName;
+    // Write your code here
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+    ]
+    dayName = days[(new Date(dateString)).getDay()];
+    return dayName;
+}
+
+
+// Day 7
