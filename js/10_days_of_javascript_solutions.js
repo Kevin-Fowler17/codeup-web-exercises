94c1f5885d46cacfe288e70a4799d3278ccb90b9
Kevin Fowler <kevin.m.fowler17@gmail.com>
2023-10-13 14:57:11 -0500

    hacker rank 10 days of javascript solutions - day 8 part 1 is done

--- js/10_days_of_javascript_solutions.js
+++ js/10_days_of_javascript_solutions.js
@@ -454,3 +454,32 @@ function regexVar() {
 
 
 // Day 8
+// Create a button
+// HTML
+<!-- Enter your HTML code here -->
+// <!DOCTYPE html>
+// <html>
+// <head>
+// <meta charset="utf-8">
+//     <link rel="stylesheet" href="css/button.css" type="text/css"
+//     <title>Button</title>
+//     </head>
+//     <body>
+//     <button id="btn">0</button>
+//
+//     <script src="js/button.js" type="text/javascript"></script>
+//     </body>
+//     </html>
+
+// CSS
+// #btn {
+//     width: 96px;
+//     height: 48px;
+//     font-size: 24px;
+// }
+let btn = document.getElementById("btn");
+
+btn.addEventListener("click", function() {
+    let current_button_value = this.innerHTML;
+    this.innerHTML = parseInt(current_button_value) + 1;
+});
