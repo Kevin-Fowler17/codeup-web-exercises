2848f95b44927151c4b7344c34bf985915a2e039
Kevin Fowler <kevin.m.fowler17@gmail.com>
2023-10-24 16:16:12 -0500

    hacker rank 1 Week Preparation - Day 2 - 3 of 3 done

--- js/Hackerrank - Mock Test.js
+++ js/Hackerrank - Mock Test.js
@@ -187,3 +187,22 @@ function diagonalDifference(arr) {
     return Math.abs(totalDiagLeftToRight - totalDiagRightToLeft);
 }
 
+/*
+ * Complete the 'countingSort' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function countingSort(arr) {
+    // Write your code here
+    let newArray = new Array(100).fill(0);
+
+    for (let i of arr) {
+        newArray[i] += 1;
+    }
+
+    return newArray;
+
+}
+
