321afd70a77adec310b0ec5e7bc2f6894164aeb1
Kevin Fowler <kevin.m.fowler17@gmail.com>
2023-10-28 10:56:13 -0500

    Hackerrank Grading Students - Done

---
+++ js/hackerrankAlgorithms.js
@@ -0,0 +1,28 @@
+/*
+ * Complete the 'gradingStudents' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY grades as parameter.
+ */
+
+function gradingStudents(grades) {
+    // Write your code here
+    let gradesRounded = [];
+
+    for (let i = 0; i < grades.length; i++) {
+        let onesPositionValue = grades[i] % 10;
+        if (grades[i] < 38){
+            gradesRounded.push(grades[i]);
+        } else if (onesPositionValue === 3 || onesPositionValue === 8) {
+            gradesRounded.push(grades[i] + 2) ;
+        } else if (onesPositionValue === 4 || onesPositionValue === 9) {
+            gradesRounded.push(grades[i] + 1);
+        } else {
+            gradesRounded.push(grades[i]);
+        }
+    }
+
+    return gradesRounded;
+}
+
+
