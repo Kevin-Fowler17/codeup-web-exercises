8b7d18e8b4c835edbf1f8124b7f8b85bfd16ff21
Kevin Fowler <kevin.m.fowler17@gmail.com>
2023-10-30 10:00:02 -0500

    Hackerrank Staircase - A Very Big Sum

--- js/hackerrankAlgorithms.js
+++ js/hackerrankAlgorithms.js
@@ -68,3 +68,16 @@ function staircase(n) {
     }
 
 }
+
+
+/*
+ * Complete the 'aVeryBigSum' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts LONG_INTEGER_ARRAY ar as parameter.
+ */
+
+function aVeryBigSum(ar) {
+    // Write your code here
+    return ar.reduce((accumulator, currValue) => accumulator + currValue, 0);
+}
